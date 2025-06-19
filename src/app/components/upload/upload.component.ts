f9d5a369fb620d73cf3fe4f6a62d332c0db28e37
Jefferson <jo_cg92@hotmail.com>
2025-06-19 14:08:05 -0500

    Update upload.component.ts

--- src/app/components/upload/upload.component.ts
+++ src/app/components/upload/upload.component.ts
@@ -6,6 +6,7 @@ import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { FileSessionService } from '../../services/file-session.service';
 import { environment } from 'src/environments/environment';
 
+
 @Component({
   selector: 'app-upload',
   standalone: true,
